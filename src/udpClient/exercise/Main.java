e1114ecf7ccfaf71aa785594b8be0ed39916ddd3
B10807013 <object10422@gmail.com>
2019-10-03 14:09:55 +0800

    practice of UDP

---
+++ src/udpClient/exercise/Main.java
@@ -0,0 +1,35 @@
+package udpClient.exercise;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class Main {
+    public static void main(String[] args){
+        try{
+            final int PORT = 8000;
+            DatagramSocket ds = new DatagramSocket();
+            InetAddress addr = InetAddress.getByName("10.2.8.162");
+            String s;
+
+            do{
+                BufferedReader keyIn = new BufferedReader(new InputStreamReader(System.in));
+                System.out.print("Your ID: ");
+                s = keyIn.readLine();
+                if(s.equals("")){
+                    break;
+                }
+                DatagramPacket dp = new DatagramPacket(s.getBytes(), s.length(), addr, PORT);
+                ds.send(dp);
+            } while(true);
+            ds.close();
+
+        } catch (Exception e){
+            System.out.print(e);
+        }
+
+
+    }
+}
